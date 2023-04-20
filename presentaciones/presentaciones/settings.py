98c17bf1fc973331174be1a94cdf74e6c79c75f6
SIRLANY.MORA <SIRLANY.MORA@ucr.ac.cr>
2023-04-20 11:07:39 -0600

    Creación de pagina miselanea para registro de usuarios

--- presentaciones/presentaciones/settings.py
+++ presentaciones/presentaciones/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'clases.apps.ClasesConfig',
     'inicio.apps.InicioConfig',
+    'registro.apps.RegistroConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
